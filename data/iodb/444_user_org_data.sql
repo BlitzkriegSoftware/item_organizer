5bbbaf6479c076e1f6d3c7b04f3c270ebf049faa
Stuart Williams <spookdejur@hotmail.com>
2026-08-29 22:54:40 -0700

    sql, reorg, 2

--- data/iodb/444_user_org_data.sql
+++ data/iodb/444_user_org_data.sql
@@ -1,7 +1,7 @@
-truncate table {setting}.user_org;
+truncate table {schema}.user_org;
 
 -- Super user
-INSERT INTO {setting}.user_org(
+INSERT INTO {schema}.user_org(
 	user_id, org_id, org_role_id)
 	VALUES ('00000000-0000-0000-0000-000000000000', 
             '00000000-0000-0000-0000-000000000000', 
