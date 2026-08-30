7d10853ceb22a3522ab721d3a370ed8c8270fc46
Stuart Williams <spookdejur@hotmail.com>
2026-08-29 22:39:51 -0700

    sides,5

--- data/iodb/146_app_configuration_set.sql
+++ data/iodb/146_app_configuration_set.sql
@@ -2,6 +2,7 @@ CREATE OR REPLACE PROCEDURE {schema}.app_configuration_set(
         name character varying(128),
         new_value character varying(128)
 )
+ LANGUAGE 'sql'
 AS $BODY$
 	update {setting}.app_configuration 
     set setting_value = new_value
